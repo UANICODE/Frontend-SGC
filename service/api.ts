3006b8d0ba03eb9f3c477b989110156ae1e52a76
blackbox <blackbox@blackbox.blackbox>
2026-03-08 02:07:36 +0200

    Comfig de proxy com vercel

--- service/api.ts
+++ service/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 15000,
   withCredentials: true, // 🔥 ESSENCIAL PARA COOKIES HTTP ONLY
 });
