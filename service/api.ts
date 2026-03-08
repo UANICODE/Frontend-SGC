a3269449eab0235f91f249266340ff7f8fc7f7c9
blackbox <blackbox@blackbox.blackbox>
2026-03-08 02:57:50 +0200

    Retorno 0

--- service/api.ts
+++ service/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 15000,
   withCredentials: true, // 🔥 ESSENCIAL PARA COOKIES HTTP ONLY
 });
